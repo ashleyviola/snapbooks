b86684b0238416c9ddcc1e7c905f8565387b0dbe
unknown <44147809+LSabin23@users.noreply.github.com>
2022-01-19 12:18:43 -0500

    added pdfkit

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const pdfRoutes = require('./pdf-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/generatePDF', pdfRoutes);
 
 module.exports = router;
